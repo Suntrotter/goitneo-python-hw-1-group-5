efd108ef4ebed0081c785cdd0c7b1fef56b0f727
Suntrotter <suntrotter@gmail.com>
2024-03-06 21:02:29 +0200

    commit

--- Address_book.py
+++ Address_book.py
@@ -19,72 +19,76 @@ class Phone(Field):
         if len(phone) == 10:
             super().__init__(phone)
         else:
-            print("Please enter a valid phone: ")
+            super().__init__(phone)
+            print("Please enter a valid phone")
 
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
-        self.phones = []
+        self.phones = ['0987654321', '1234567890']
 
     def add_phone(self, phone):
-        phone = Phone(phone)
         self.phones.append(phone)
+        return self.phones
 
-    def edit_phone(self, new_phone):
-        for phone in self.phones:
-            if phone.value == new_phone:
-                phone = new_phone
-                return self.phones
+    def edit_phone(self, phone, new_phone):
+        for i in range(len(self.phones)):
+            if self.phones[i] == phone:
+                self.phones[i] = new_phone
+        return self.phones
 
     def remove_phone(self, phone):
-        for phone in self.phones:
-            if phone.value == phone:
-                self.phones.remove(phone)
-                return self.phones
+        if phone in self.phones:
+            self.phones.remove(phone)
+            return self.phones
 
     def find_phone(self, phone):
-        for phone in self.phones:
-            if phone.value == phone:
-                return f"{name}: {phone}"
+        if phone in self.phones:
+            string = f"{phone}"
+            return string
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+        string = f"Contact name: {self.name}, phones: {'; '.join(p for p in self.phones)}"
+        return string
 
 
 class AddressBook(UserDict):
     def __init__(self):
         super().__init__()
 
-    def handle_add(name, phone, book):
-        record = Record(name)
-        record.add_phone(phone)
-        book.add_record(record)
-        return book
-
     def add_record(self, record):
-        self.data.update(record)
+        self.data.update({record.name.value: record})
         return self.data
 
     def find_record(self, name):
         for record in self.data:
-            if record.name.value == name:
-                return record
+            if name in record:
+                return self.data[record]
         return "Not Found"
 
     def delete_record(self, name):
+
         for record in self.data:
-            if record.name.value == name:
-                self.data.remove(record)
-            else:
-                return "Unable to delete"
+            if name in record:
+                del self.data[record]
+                return self.data
+        return "Unable to delete"
+
+
+def handle_add(name, phone, book):
+    record = Record(name)
+    record.add_phone(phone)
+    book.add_record(record)
+    print(book)
+    return book
 
 
 def main():
     book = AddressBook()
     name = input("Please enter a name: ")
     phone = input("Please enter a phone: ")
-    AddressBook.handle_add(name, phone, book)
+    handle_add(name, phone, book)
 
 
 if __name__ == '__main__':
